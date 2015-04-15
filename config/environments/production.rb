ed4a3ae41ee13010f515aa424c52b99557ed1abe
Shenmin <shenminzhou@gmail.com>
2015-04-15 10:24:13 -0500

    email fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ AppSys::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'apply-scottyshouse.herokuapp.com'
+  host = 'scottyshouse-apply.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
